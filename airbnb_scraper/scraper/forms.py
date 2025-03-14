f98af269f1e4d61723aae30a838ca81f82723b5b
ba306 <bogacaybey@gmail.com>
2025-03-14 17:46:58 +0100

    added email box, changed currency

--- airbnb_scraper/scraper/forms.py
+++ airbnb_scraper/scraper/forms.py
@@ -1,11 +1,13 @@
 from django import forms
+from django.core.validators import EmailValidator
 
 class SearchForm(forms.Form):
     start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     num_adults = forms.IntegerField(min_value=1, max_value=10)
     max_price = forms.IntegerField(min_value=0)
-    ne_lat = forms.FloatField()
-    ne_lng = forms.FloatField()
-    sw_lat = forms.FloatField()
-    sw_lng = forms.FloatField()
+    email = forms.CharField(validators=[EmailValidator()], widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'}))
+    ne_lat = forms.FloatField(widget=forms.HiddenInput())  # Hidden field
+    ne_lng = forms.FloatField(widget=forms.HiddenInput())  # Hidden field
+    sw_lat = forms.FloatField(widget=forms.HiddenInput())  # Hidden field
+    sw_lng = forms.FloatField(widget=forms.HiddenInput())  # Hidden field
