99be72c30d06d81af091b7bafe6697c9896dd3fa
ba306 <bogacaybey@gmail.com>
2025-04-11 23:11:38 +0200

    remove previous json url loadings and savings -- structures are saved to SQLite3

--- airbnb_scraper/scraper/utils.py
+++ airbnb_scraper/scraper/utils.py
@@ -12,18 +12,6 @@ def load_credentials():
         print("Config file not found!")
         return None, None
 
-# Load previously fetched URLs
-def load_previous_urls():
-    previous_urls = []
-    for search in SearchHistory.objects.all():
-        previous_urls.extend(search.results)
-    return previous_urls
-
-# Save URLs to a file
-def save_urls(urls, file_path="previous_urls.json"):
-    with open(file_path, "w") as f:
-        json.dump(urls, f)
-
 def normalize_url(url):
     """Normalize the URL by removing query parameters and trailing slashes."""
     parsed_url = urlparse(url)
